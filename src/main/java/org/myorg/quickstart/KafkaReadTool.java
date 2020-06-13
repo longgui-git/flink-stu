dc62f77f4bc01fca2f0221e1ce2eff1233413a5a
wanghao83 <mwh151543@163.com>
2020-06-13 13:19:33 +0800

    test git push origin HEAD:refs/for/master

--- src/main/java/org/myorg/quickstart/KafkaReadTool.java
+++ src/main/java/org/myorg/quickstart/KafkaReadTool.java
@@ -34,7 +34,6 @@ public class KafkaReadTool {
 
 	public static void main(String[] args) throws Exception {
 
-        // perception_out_test_1390515961993707490
 		String debugCrossId = "1390515961993707490";
 		ParameterTool parameterTool = ParameterTool.fromArgs(args);
 
